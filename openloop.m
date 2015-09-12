be825589d142070b050ad9d17cb347eaf7bb6150
slremy <sekouremy@gmail.com>
2015-09-12 08:55:15 -0400

    adding simple matlab program

---
+++ openloop.m
@@ -0,0 +1,37 @@
+%{
+         Copyright (c) 2015 Sekou L. Remy
+ 
+         Permission is hereby granted, free of charge, to any person obtaining a copy
+         of this software and associated documentation files (the "Software"), to deal
+         in the Software without restriction, including without limitation the rights
+         to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+         copies of the Software, and to permit persons to whom the Software is
+         furnished to do so, subject to the following conditions:
+ 
+         The above copyright notice and this permission notice shall be included in
+         all copies or substantial portions of the Software.
+ 
+         THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+         IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+         FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+         AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+         LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+         OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+         THE SOFTWARE.
+
+Test program that turns and moves repeatedly.
+%}
+
+temp_base_url = 'http://somehost:someport/verb?';
+permissions = '&id=somevalue';
+
+while 1
+    for i=1:50
+        urlread([temp_base_url,permissions,'&az=',num2str(-.9)]); %turn(-.9);
+        pause(.1)
+    end
+    for i=1:100
+        urlread([temp_base_url,permissions,'&lx=',num2str(.1)]); %moveForward(.1)
+        pause(.1)
+    end
+end
